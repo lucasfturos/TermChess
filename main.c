ebc6d6e1ede5e30826bbd3d13654a3b8b5202f91
lucasfturos <lucasfturos@gmail.com>
2023-06-28 21:00:37 -0300

    Otimizando a função do tabuleiro

--- main.c
+++ main.c
@@ -6,31 +6,37 @@
 #define COLOR_CLEAR "\e[0m"
 #define COLOR_BLACK "\e[40m"
 #define COLOR_WHITE "\e[47m"
+#define COLOR_RED "\e[41m"
 
-int board[SIZE][SIZE];
-char piece_white[][5] = {" ♔", " ♕", " ♖", " ♗", " ♘", " ♙"};
-char piece_black[][5] = {" ♚", " ♛", " ♜", " ♝", " ♞", " ♟"};
+char piece_white[][5] = {" ♙", " ♖", " ♘", " ♗", " ♕", " ♔"};
+char piece_black[][5] = {" ♟", " ♜", "♞", " ♝", " ♛", " ♚"};
 
-void printBoard() {
-    puts("    A  B  C  D  E  F  G  H");
+int board[SIZE][SIZE] = {{2, 3, 4, 5, 6, 4, 3, 2}, {1, 1, 1, 1, 1, 1, 1, 1},
+                         {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+                         {1, 1, 1, 1, 1, 1, 1, 1}, {2, 3, 4, 5, 6, 4, 3, 2}};
+
+void printBoard(int x, int y) {
+    puts("\033c");
+    puts("    a  b  c  d  e  f  g  h");
     for (size_t row = SIZE; row >= 1; row--) {
         printf("%s %zu ", COLOR_CLEAR, row);
         for (size_t col = 0; col < SIZE; col++) {
-            if (row % 2) {
-                col % 2 ? printf("%s   ", COLOR_BLACK)
-                        : printf("%s   ", COLOR_WHITE);
+            if ((row - 1) == x && col == y) {
+                printf("%s%s  ", COLOR_BLACK, piece_black[2]);
+            } else if ((row + col) % 2 == 0) {
+                printf("%s   ", COLOR_WHITE);
             } else {
-                col % 2 ? printf("%s   ", COLOR_WHITE)
-                        : printf("%s   ", COLOR_BLACK);
+                printf("%s   ", COLOR_BLACK);
             }
         }
-        printf("%s %zu", COLOR_CLEAR, row );
+        printf("%s %zu", COLOR_CLEAR, row);
         putchar('\n');
     }
-    puts("    A  B  C  D  E  F  G  H");
+    puts("    a  b  c  d  e  f  g  h");
 }
 
 int main() {
-    printBoard();
+    printBoard(0, 0);
     return 0;
 }
